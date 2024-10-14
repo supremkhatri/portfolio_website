7d52e05396af836e8159e07d007b207e58c0895b
supremkhatri <suprem.khatri708@gmail.com>
2024-10-14 12:43:53 +0545

    Scroll resolve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Navbar from "@/components/Navbar";
 import HeroSection_1 from "@/components/HeroSection_1";
 import HeroSection_2 from "@/components/HeroSection_2";
@@ -8,17 +8,9 @@ import Experience from "@/components/Experience";
 import Timeline from "@/components/Timeline";
 import Footer from "@/components/FooterSection";
 import { Merriweather } from "next/font/google";
-import { useEffect } from 'react';
-
-
-
 
 export default function Home() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
   return (
-    
     <main className={merri.className}>
       <Navbar className="border border-white " />
       <HeroSection_1 />
@@ -29,7 +21,6 @@ export default function Home() {
       <Timeline />
       <Footer />
     </main>
-  
   );
 }
 
