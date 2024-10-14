5349cd633138817c1d292ad29bd878035aabe63a
supremkhatri <suprem.khatri708@gmail.com>
2024-10-14 16:18:27 +0545

    Final edits

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,13 +19,6 @@ export default function CardHoverEffect() {
 
 export const projects = [
   {
-    title: "To do App",
-    description:
-      "This project is built using JavaScript, along with basic HTML and pure CSS. The application allows users to store, delete, and edit their to-do items.",
-    link: "https://supremkhatri.github.io/todotracker/",
-    imageUrl: "/images/project/todo-icon.png",
-  },
-  {
     title: "Dictionary App",
     description: "This project leverages JavaScript, basic HTML, and Tailwind CSS to create a dynamic dictionary app. The application enables users to search for word definitions by utilizing a free dictionary API, providing a seamless and efficient word lookup experience.",
     link: "https://supremkhatri.github.io/dictionary/",
@@ -42,5 +35,12 @@ export const projects = [
     description: "This game is developed using JavaScript, HTML, and pure CSS to bring the classic scissor-paper-rock game to life. Users can challenge a bot in this interactive app, with the game automatically determining the winner after each round.",
     link: "https://supremkhatri.github.io/scissorpaperrock/",
     imageUrl: "/images/project/rock-paper-scissor.jpg",
+  },
+  {
+    title: "To do App",
+    description:
+      "This project is built using JavaScript, along with basic HTML and pure CSS. The application allows users to store, delete, and edit their to-do items.",
+    link: "https://supremkhatri.github.io/todotracker/",
+    imageUrl: "/images/project/todo-icon.png",
   },
 ];
